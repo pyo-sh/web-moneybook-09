e3c38ea8d650da47f1a50449e1c59bd2f724e953
pyo-sh <pygosky@gmail.com>
2022-07-23 02:09:16 +0900

    :sparkles: Router를 통해 App 의 컴포넌트들 라우팅

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import Component from "@core/Component";
 import { div } from "@core/CreateDom";
 import Header from "@components/Header";
+import Main from "@pages/Main";
+import Calendar from "@pages/Calendar";
+import Statistic from "@pages/Statistic";
+import { pageState, Routes } from "@core/Route";
 
 export default class App extends Component {
+    bindState() {
+        return [pageState];
+    }
+
     render() {
-        return div({ id: "root" })(new Header());
+        return div({
+            id: "root",
+        })(
+            new Header(),
+            Routes({ Component: Main, path: "/" }),
+            Routes({ Component: Calendar, path: "/calendar" }),
+            Routes({ Component: Statistic, path: "/statistic" }),
+        );
     }
 }
