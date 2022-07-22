876cfd1358cf4ad65d929c50100992b12b2e73b8
pyo-sh <pygosky@gmail.com>
2022-07-23 01:58:05 +0900

    :sparkles: 대표적인 페이지들의 컴포넌트 생성 #10

---
+++ client/src/pages/Main.js
@@ -0,0 +1,8 @@
+import Component from "@core/Component";
+import { div } from "@core/CreateDom";
+
+export default class Main extends Component {
+    render() {
+        return div("Main");
+    }
+}
