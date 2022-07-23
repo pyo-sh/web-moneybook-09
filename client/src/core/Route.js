b9000d76098d2da35869a0e1cc06d8af6eac3241
pyo-sh <pygosky@gmail.com>
2022-07-23 14:48:29 +0900

    :recycle: Route의 사용을 명확하게 표기했습니다 #11

--- client/src/core/Route.js
+++ client/src/core/Route.js
@@ -7,7 +7,12 @@ export const pageState = makeObservable({
 export const pageList = new Map();
 
 const Route = {
-    push(page) {
+    navigate(page) {
+        if (!page) {
+            pageState.page = getPageFromPath(window.location.pathname);
+            return;
+        }
+
         if (pageState !== page) {
             history.pushState(null, null, page);
             pageState.page = page;
@@ -15,12 +20,15 @@ const Route = {
             history.replaceState(null, null, page);
         }
     },
-    pop() {
-        pageState.page = getPageFromPath(window.location.pathname);
-    },
 };
-window.onpopstate = Route.pop;
+window.onpopstate = () => Route.navigate();
 
+/**
+ * 컴포넌트와 경로를 받아서 라우팅합니다.
+ * 컴포넌트의 경로가 맞을 때 Element를, 아닐 때 null을 반환합니다.
+ * @param {{ Component: Class, path: string }} 라우팅 할 컴포넌트의 정보들
+ * @returns HTMLElement | null
+ */
 export const Routes = ({ Component, path }) => {
     if (!Component) return null;
     if (`/${pageState.page}` === path) return new Component();
