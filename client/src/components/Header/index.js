e980251d6f24523c07dc298608fe311c7fa639bf
pyo-sh <pygosky@gmail.com>
2022-07-25 01:06:28 +0900

    :sparkles: Header 에서 controlDate 전역 객체 변경 로직 작성 #19

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -4,6 +4,7 @@ import { a, article, button, div, h1, h2, header, section, span } from "@core/Cr
 import { fileTextIcon, calendarIcon, chartIcon } from "@icons";
 import downArrowIcon from "@icons/downArrow";
 import Route from "@core/Route";
+import controlDate from "@store/controlDate";
 
 const WHITE = "#ffffff";
 
@@ -16,8 +17,25 @@ export default class Header extends Component {
             Route.navigate(pathname);
         }
     }
+    onClickMonthMover({ target }) {
+        const monthMover = target.closest(".monthMover");
+        if (!monthMover) {
+            return false;
+        }
+
+        const { id } = monthMover;
+        if (id === "monthPrevMover") {
+            controlDate.movePrev();
+        }
+        if (id === "monthNextMover") {
+            controlDate.moveNext();
+        }
+    }
     render() {
-        const { onClickATag } = this;
+        const { onClickATag, onClickMonthMover } = this;
+        const currentDate = controlDate.state.value;
+        const month = currentDate.getMonth() + 1;
+        const year = currentDate.getFullYear();
         // prettier-ignore
         return header({ class: "header" })(
             div({
@@ -27,13 +45,18 @@ export default class Header extends Component {
                 }})
             (
                 a({ href: "/" })(h1({ class: "text_display_small" })("우아한 가계부")),
-                section({ class: "controller" })(
-                    button({ class: "monthMover" })(downArrowIcon(WHITE, 24, 24)),
+                section({
+                    class: "controller",
+                    event: {
+                        click: onClickMonthMover
+                    }
+                })(
+                    button({ id: "monthPrevMover", class: "monthMover" })(downArrowIcon(WHITE, 24, 24)),
                     article(
-                        h2({ class: "text_display_large" })("7월"),
-                        span({ class: "text_display_small" })("2021")
+                        h2({ class: "text_display_large" })(`${month}월`),
+                        span({ class: "text_display_small" })(year)
                     ),
-                    button({ class: "monthMover" })(downArrowIcon(WHITE, 24, 24)),
+                    button({ id: "monthNextMover", class: "monthMover" })(downArrowIcon(WHITE, 24, 24)),
                 ),
                 section({ class: "pageTabs" })(
                     a({ href: "/" })(fileTextIcon(WHITE)),
