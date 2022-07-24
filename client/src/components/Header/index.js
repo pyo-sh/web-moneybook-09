6d5dd574d198582e8bd415ee99f291f98a219a2d
pyo-sh <pygosky@gmail.com>
2022-07-24 21:42:08 +0900

    :sparkles: Header 에서 A Tag 클릭 시 페이지 이동 구현 #19

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -3,14 +3,29 @@ import "@components/Header/index.css";
 import { a, article, button, div, h1, h2, header, section, span } from "@core/CreateDom";
 import { fileTextIcon, calendarIcon, chartIcon } from "@icons";
 import downArrowIcon from "@icons/downArrow";
+import Route from "@core/Route";
 
 const WHITE = "#ffffff";
 
 export default class Header extends Component {
+    onClickATag(e) {
+        const aElement = e.target.closest("a");
+        if (aElement) {
+            e.preventDefault();
+            const pathname = aElement.pathname.replace("/", "");
+            Route.navigate(pathname);
+        }
+    }
     render() {
+        const { onClickATag } = this;
         // prettier-ignore
         return header({ class: "header" })(
-            div({ class: "wrapper"})(
+            div({
+                class: "wrapper",
+                event: {
+                    click: onClickATag
+                }})
+            (
                 a({ href: "/" })(h1({ class: "text_display_small" })("우아한 가계부")),
                 section({ class: "controller" })(
                     button({ class: "monthMover" })(downArrowIcon(WHITE, 24, 24)),
