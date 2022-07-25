142f3e054a0a6ebd21de41fffd3a1aeb03fd3a38
youngkyo0504 <keumky2@gmail.com>
2022-07-25 21:35:42 +0900

    🎉 price amount input 구현
    - minus클릭시 plus 아이콘으로 변함
    - 애니메이션이 끝나면 isIncome 상태 변경함

---
+++ client/src/components/AccountForm/AmountInput.js
@@ -0,0 +1,48 @@
+import { div, label, button, input, span } from "@core/CreateDom";
+import { formatAmount } from "./validation";
+
+const AmountInput = ({ ref, state }) => {
+    const toggleIsIncome = () => {
+        ref.category = null;
+        state.isIncome = !state.isIncome;
+    };
+
+    const transitionIcon = (e) => {
+        e.stopPropagation();
+        e.currentTarget.classList.toggle("form-payment-sign-plus", !state.isIncome);
+    };
+
+    const setAmount = ({ currentTarget }) => {
+        currentTarget.value = formatAmount(currentTarget.value); //문자열
+        ref.amount = currentTarget.value.replace(/,/g, "");
+        currentTarget.dispatchEvent(new Event("validate", { bubbles: true }));
+        console.log(ref.amount);
+    };
+
+    return div({ class: "inputBox" })(
+        div({ class: "inputItem price" })(
+            label({ class: "text_bold_small label" })("금액"),
+            div({ class: `priceInputContainer` })(
+                button({
+                    type: "button",
+                    class: `form-payment-sign ${state.isIncome ? "form-payment-sign-plus" : ""}`,
+                    event: {
+                        click: transitionIcon,
+                        transitionend: toggleIsIncome,
+                    },
+                })(),
+                input({
+                    class: "text_body_regular",
+                    placeholder: "입력하세요",
+                    value: formatAmount(ref.amount) || "",
+                    event: {
+                        input: setAmount,
+                    },
+                })(),
+                span("원"),
+            ),
+        ),
+    );
+};
+
+export default AmountInput;
