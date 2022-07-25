026527b8fe992f0b88f8ceae9aa7f18bfa6b3216
pyo-sh <pygosky@gmail.com>
2022-07-25 16:43:16 +0900

    :sparkles: Category Model 중 CRUD Queries 기능 추가 #25

---
+++ server/models/category.js
@@ -0,0 +1,46 @@
+const pool = require("../db/loader");
+const { getCreateQuery, getUpdateQuery, getDeleteQuery } = require("../utils/query");
+const { formatPropertyToSnake, formatPropertyToCamel } = require("../utils/format");
+
+module.exports = (function CategoryModel() {
+    const TABLE_NAME = "category";
+    const COLUMNS = ["id", "name", "color", "is_income"];
+    const TABLE_INFO = {
+        tableName: TABLE_NAME,
+        columns: COLUMNS,
+    };
+
+    async function create({ data }) {
+        const pureData = { ...data };
+        data = formatPropertyToSnake(data);
+        const query = getCreateQuery(TABLE_INFO, data);
+        const [fields] = await pool.execute(query);
+        return {
+            id: fields.insertId,
+            ...pureData,
+        };
+    }
+
+    async function findAll() {
+        const query = `
+            SELECT *
+            FROM ${TABLE_NAME}
+        `;
+        const [rows] = await pool.execute(query);
+        return rows.map(formatPropertyToCamel);
+    }
+
+    async function updateById({ id, data }) {
+        data = formatPropertyToSnake(data);
+        const query = getUpdateQuery(TABLE_INFO, id, data);
+        const [rows] = await pool.execute(query);
+        return rows[0];
+    }
+
+    async function deleteById({ id }) {
+        const query = getDeleteQuery(TABLE_INFO, id);
+        return await pool.execute(query);
+    }
+
+    return { create, findAll, updateById, deleteById };
+})();
