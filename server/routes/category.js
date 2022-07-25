b7a1db1d15bd0778eed3b9d56358a0397d30999f
pyo-sh <pygosky@gmail.com>
2022-07-25 17:30:45 +0900

    :sparkles: Category API 실행 중 try catch 로 예외 오류 처리 #25

--- server/routes/category.js
+++ server/routes/category.js
@@ -2,28 +2,44 @@ const express = require("express");
 const router = express.Router();
 const CategoryService = require("../services/category");
 
-router.get("/", async (req, res) => {
-    const categories = await CategoryService.getCategoryAll();
-    return res.send(categories);
+router.get("/", async (req, res, next) => {
+    try {
+        const categories = await CategoryService.getCategoryAll();
+        return res.send(categories);
+    } catch (e) {
+        next(e);
+    }
 });
 
-router.post("/", async (req, res) => {
-    const { body } = req;
-    const data = await CategoryService.addCategory(body);
-    return res.send(data);
+router.post("/", async (req, res, next) => {
+    try {
+        const { body } = req;
+        const data = await CategoryService.addCategory(body);
+        return res.send(data);
+    } catch (e) {
+        next(e);
+    }
 });
 
-router.patch("/:id", async (req, res) => {
-    const { id } = req.params;
-    const { body } = req;
-    const data = await CategoryService.editCategory(id, body);
-    return res.send(data);
+router.patch("/:id", async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { body } = req;
+        const data = await CategoryService.editCategory(id, body);
+        return res.send(data);
+    } catch (e) {
+        next(e);
+    }
 });
 
-router.delete("/:id", async (req, res) => {
-    const { id } = req.params;
-    await CategoryService.deleteCategory(id);
-    return res.send({ message: "Successfully Deleted" });
+router.delete("/:id", async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        await CategoryService.deleteCategory(id);
+        return res.send({ message: "Successfully Deleted" });
+    } catch (e) {
+        next(e);
+    }
 });
 
 module.exports = router;
