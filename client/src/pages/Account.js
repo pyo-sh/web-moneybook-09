cdd047bca4e7e81291249c86338df36e2bc4c1e0
youngkyo0504 <keumky2@gmail.com>
2022-07-25 21:53:38 +0900

    🎉 account form app.js에 추가 및 글로벌 스타일 적용

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -1,8 +1,18 @@
 import Component from "@core/Component";
+import AccountForm from "@src/components/AccountForm";
+import Modal from "@components/Modal";
 import { div } from "@core/CreateDom";
-
 export default class Account extends Component {
     render() {
-        return div("Account");
+        console.log("Account Render");
+        // prettier-ignore
+        return div(
+                new AccountForm(),
+                new Modal()
+        );
     }
 }
+
+// 2 가지 방법
+// 따로 id를 만들거나 ,
+// 엔트리 포인트를 그리거나
