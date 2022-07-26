f34969645328caacbd805ca5180f81018d2606d0
pyo-sh <pygosky@gmail.com>
2022-07-26 18:07:21 +0900

    :bug: 프론트 History API Fetch 중 getHistoriesByMonth 의 파라미터 명확히 하기 #24

--- client/src/apis/history.js
+++ client/src/apis/history.js
@@ -1,6 +1,6 @@
 import request from "@utils/request";
 
-export const getHistoriesByMonth = async (date) => {
-    const data = await request.get({ url: `/history`, query: { date } });
+export const getHistoriesByMonth = async ({ query }) => {
+    const data = await request.get({ url: `/history`, query });
     return data;
 };
