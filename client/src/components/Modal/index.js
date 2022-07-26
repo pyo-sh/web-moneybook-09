6fca41bd3653ff4710c1625eb4b5bbd2384b2b2e
youngkyo0504 <keumky2@gmail.com>
2022-07-26 18:45:16 +0900

    ♻️ modal 전역 상태 리펙토링 #20
    - type을 통해 어떤 모달을 랜더링할 지 정함
    - value를 통해 그 id값을 전달한다.

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -1,22 +1,22 @@
 import "./modal.css";
 import Component from "@core/Component";
 import { div } from "@core/CreateDom";
-import { modalState } from "@store/modalState";
-import CategoryDeleteForm from "./CategoryDeleteForm";
-import CategoryAddForm from "./CategoryAddForm";
+import modal from "@store/modal";
+import PaymentMethodDeleteForm from "./PaymentMethodDeleteForm";
+import PaymentMethodAddForm from "./PaymentMethodAddForm";
 
 const modalMap = {
-    delete: CategoryDeleteForm,
-    add: CategoryAddForm,
+    delete: PaymentMethodDeleteForm,
+    add: PaymentMethodAddForm,
 };
 
 export default class Modal extends Component {
     bindState() {
-        return [modalState];
+        return [modal.state];
     }
 
     render() {
-        const { value, type } = modalState;
+        const { value, type } = modal.state;
 
         if (!type) {
             return div();
