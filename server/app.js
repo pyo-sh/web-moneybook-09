43be811b42fb3d6586ea98141b28604935c4b2ad
pyo-sh <pygosky@gmail.com>
2022-07-26 10:56:23 +0900

    :heavy_plus_sign: DEV 환경에서 webpack-dev-server 로 인해 cors 문제를 Package 로 해결 #24

--- server/app.js
+++ server/app.js
@@ -3,11 +3,15 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 require("dotenv").config();
 require("./db/loader");
 
 const app = express();
 
+if (process.env.NODE_ENV === "development") {
+    app.use(cors());
+}
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
