97090290b38067aa3b47f392a7afcce6fb2cb3c2
pyo-sh <pygosky@gmail.com>
2022-07-26 10:58:23 +0900

    :sparkles: Date 객체를 통해 yyyy.mm 문자열을 얻을 수 있는 함수 작성 #24

--- client/src/utils/date.js
+++ client/src/utils/date.js
@@ -3,3 +3,9 @@ export const changeDateMonth = (currentDate, amount) => {
     movedDate.setMonth(currentDate.getMonth() + amount);
     return movedDate;
 };
+
+export const getYearMonth = (currentDate) => {
+    return `${currentDate.getFullYear()}.${(currentDate.getMonth() + 1)
+        .toString()
+        .padStart(2, "0")}`;
+};
