690eeb5a6641eefca5ee174f4155318d653138bd
pyo-sh <pygosky@gmail.com>
2022-07-26 21:21:47 +0900

    :lipstick: 최상위 엘리먼트 스타일링 #24

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import "@src/App.css";
 import Component from "@core/Component";
+import "@src/App.css";
 import { div } from "@core/CreateDom";
+import { pageState, Routes } from "@core/Route";
 import Header from "@components/Header";
-import Account from "@src/pages/Account";
+import Account from "@pages/Account";
 import Calendar from "@pages/Calendar";
 import Statistic from "@pages/Statistic";
-import { pageState, Routes } from "@core/Route";
 import controlDate from "@store/controlDate";
 import histories from "@store/histories";
 
@@ -19,7 +19,7 @@ export default class App extends Component {
             id: "root",
         })(
             new Header(),
-            div({ class: "AppContainer" })(
+            div({ class: "appContainer" })(
                 Routes({ Component: Account, path: "/" }),
                 Routes({ Component: Calendar, path: "/calendar" }),
                 Routes({ Component: Statistic, path: "/statistic" }),
