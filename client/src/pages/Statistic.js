562bfa2de9433138c5d749f2e72718cdc3949615
youngkyo0504 <keumky2@gmail.com>
2022-07-28 10:59:36 +0900

    ✨ payment report 생성 완료 #43
    - statistic 테이블로  데이터가 없어도 0%로 랜더링
    - 데이터가 없어도 회색으로 차트 랜더링

--- client/src/pages/Statistic.js
+++ client/src/pages/Statistic.js
@@ -1,22 +1,12 @@
 import Component from "@core/Component";
 import { section } from "@core/CreateDom";
-import DoughnutChart from "@components/DoughnutChart";
-import histories from "@store/histories";
 import "./Statistic.css";
+import PaymentReport from "@components/PaymentReport";
+
 export default class Statistic extends Component {
     getSum() {}
 
     render() {
-        const historySumMap = histories.groupPaymentSumByCategory();
-
-        if (!historySumMap) {
-            return "";
-        }
-
-        const totalExpenditure = Object.values(historySumMap).reduce((prev, cur) => prev + cur, 0);
-
-        return section({ class: "statistic" })(
-            new DoughnutChart({ totalExpenditure, historySumMap }),
-        );
+        return section({ class: "statisticWrapper" })(new PaymentReport());
     }
 }
